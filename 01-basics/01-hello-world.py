88a1872f8d1f98df03b1dbb6ffa5c8871297d7e6
JaiderSanchez <jaiderfabiansd@gmail.com>
2026-08-30 00:42:18 -0500

    Add Hello World examples in five languages

---
+++ 01-basics/01-hello-world.py
@@ -0,0 +1,14 @@
+# Español
+print("Hola, mundo!" + " ¡Bienvenido a Python!")
+
+# Inglés
+print("Hello world!" + " Welcome to Python!")
+
+# Portugués
+print("Olá, mundo!" + " Bem-vindo ao Python!")
+
+# Italiano
+print("Ciao, mondo!" + " Benvenuto in Python!")
+
+# Japonés
+print("こんにちは、世界！" + " Pythonへようこそ!") # Kon'nichiwa, sekai!'+ Paison e yōkoso!
